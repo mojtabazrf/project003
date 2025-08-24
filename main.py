ffd00db8f578a05c1431a32c21164583b17e0027
Mojtaba <mojtabazarifi86@gmail.com>
2025-08-24 15:41:14 +0330

    first step. create modular version.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from routers import instructor, student, course, enrollment
 
 app = FastAPI(title="Course Selling API")
 
-# اضافه کردن روترها
 app.include_router(instructor.router)
 app.include_router(student.router)
 app.include_router(course.router)
@@ -11,4 +10,4 @@ app.include_router(enrollment.router)
 
 @app.get("/")
 def home():
-    return {"message": "Course Selling API is running 🚀"}
+    return {"message": "Course Selling API is running"}
